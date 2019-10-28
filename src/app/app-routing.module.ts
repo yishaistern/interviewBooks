7a65577536e79cc84ddf33c512160f5d81deaf8b
yishaistern <yishainstern@gmail.com>
2019-10-28 23:16:06 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SearchComponent } from './components/search/search.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'books',
+    component: SearchComponent,
+  }, {
+    path: 'cart',
+    component: WishlistComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
