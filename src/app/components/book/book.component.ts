04272d80fa61b8b014e99433e65c49a4b84b8614
yishaistern <yishainstern@gmail.com>
2019-10-30 07:26:41 +0200

    erw

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Book } from '../../shared-objects/books-objects';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { BookInfoComponent } from '../book-info/book-info.component';
 
 @Component({
   selector: 'app-book',
@@ -9,12 +11,18 @@ import { Book } from '../../shared-objects/books-objects';
 export class BookComponent implements OnInit {
   @Input() book: Book;
   @Input() isCart: boolean;
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
     console.log(this.book);
   }
   getTitle() {
     return (this.book && this.book.volumeInfo && this.book.volumeInfo.title) ? this.book.volumeInfo.title : '';
+  }
+  openDialod() {
+    const dialogRef = this.dialog.open(BookInfoComponent, {
+      width: '250px',
+      data: {bok: this.book}
+    });
   }
 }
